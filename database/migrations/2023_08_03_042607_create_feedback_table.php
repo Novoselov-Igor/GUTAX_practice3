67e4252474d25547dcb8b7c5600c3e4e43d2d163
Novoselov-Igor <barszx23@gmail.com>
2023-08-03 08:41:09 +0400

    add migrations

--- database/migrations/2023_08_03_042607_create_feedback_table.php
+++ database/migrations/2023_08_03_042607_create_feedback_table.php
@@ -11,14 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('feedback', function (Blueprint $table) {
             $table->id();
-            $table->string('fio');
-            $table->string('email')->unique();
-            $table->string('phone');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('id_city')->constrained('cities');
+            $table->string('title');
+            $table->text('text');
+            $table->tinyInteger('rating');
+            $table->string('img');
+            $table->foreignId('id_author')->constrained('users');
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('feedback');
     }
 };
